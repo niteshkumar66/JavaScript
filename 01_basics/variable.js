a7c5166b5d6a75864ce57e43c737cd435bb7517a
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-03 09:25:12 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,24 @@
+const accountId = 1445533
+let accountEmail = "nitesh@gmail.com"
+var accountPass = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 
+
+accountEmail = "ni@ni.com"
+accountPass = "12321"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+/* 
+Prefer not to use var 
+Because of issue in block scope and functional scope
+*/
+
+console.table([accountId , accountEmail , accountPass , accountCity , accountState])
+
+
+
+ 
