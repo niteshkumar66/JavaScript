c9fb051d042db38e1f7b9bdec164b5d8daae8110
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-03 09:45:45 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // all JS codes are treated as newer version 
+
+// alert ("hello") // we are using nodejs not browser
+
+/* console.log(3 
+    + 
+    3 );  // code readability should be high
+*/
+
+let name = " Nitesh kumar "
+let age = 18
+let isloggedIn = true
+let state = null
+let city;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined => value not defined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
