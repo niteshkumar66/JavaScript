79cd940767bab5522979a3e77703257476988119
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-03 11:42:35 +0000

    conversionsOperations

---
+++ 01_basics/conversionsOperations.js
@@ -0,0 +1,33 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number) 
+// true => 1 ; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// true => 1 ; false => 0
+// "" => false
+// "nitesh" => true
+
+
+let sum = 10
+let stringNumber = String(sum)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
