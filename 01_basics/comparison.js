018dd71df8018f30a09997d830b090d4760bfad8
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-04 20:38:01 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1); // JS automatically convert the string to number for the comparison 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
