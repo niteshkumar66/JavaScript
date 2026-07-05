848a4b5d20439e9158a00a50924a2a8023c31a2b
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-05 19:22:16 +0000

    dates

---
+++ 01_basics/datesinJS.js
@@ -0,0 +1,17 @@
+const myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toTimeString())
+
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date (2026, 0, 23) // month start from 0 to 11 in JS 
+console.log(myCreatedDate.toDateString());
+
