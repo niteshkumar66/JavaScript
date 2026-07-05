525a0a5914bd131b1e8d7c90b09c460778f14a9f
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-05 09:11:24 +0000

    comparison

--- 01_basics/comparison.js
+++ 01_basics/comparison.js
@@ -9,9 +9,27 @@
 // console.log("2" > 1);
 // console.log("02" > 1); // JS automatically convert the string to number for the comparison 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null < 0);
-console.log(null >= 0);
-console.log(null <= 0);
+// console.log(null > 0); //null converts into 0 for comparison other than ==
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
 
+// console.log(undefined == 0); // undefined is neither == , < , > than no.
+// console.log(undefined < 1);
+// console.log(undefined > 1);
+
+/* 
+    === compares the entity as well the datatype 
+*/
+
+// console.log("2" === 2); 
+
+/* other comparison operator other than === converts the datatype automatically for the better comparison like true converts into 1 , null into 0 
+*/
+
+// console.log(true == "1");
+// console.log(true === "1");
+// console.log(true === 1);
+
+/* comparison of different datatype is not good practice for the coding */
