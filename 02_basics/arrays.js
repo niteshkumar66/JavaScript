0ade7a50e89e12bd3ac11ef8783fe35aac06bbb7
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-06 08:39:39 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,62 @@
+// Arrays 
+
+const myArr = [1,2,3,4,5,6, true , "nitesh"];
+
+// console.log(myArr[0])
+
+const newArr = myArr
+
+// console.log(newArr);
+
+// newArr[2] = "raju"
+// console.log(newArr);
+// console.log(myArr)    
+
+const arr2 = new Array(1,2,3,"hero","baacha")
+// console.log(arr2);
+
+
+// Array Methods =>
+
+
+// console.log(arr2.reverse());
+// console.log(arr2.indexOf(2));
+// console.log(arr2.toString());
+
+// myArr.push(5) 
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes('9'));
+
+
+// const Arr3 = myArr.join()
+
+
+// console.log(myArr);
+// console.log(Arr3);
+
+
+// slice , splice 
+
+console.log("A " , myArr);
+
+const myn1 = myArr.slice(1,5)   // (starting , ending-1) or (starting)
+
+
+console.log(myn1)
+console.log("B ", myArr);  
+
+
+const myn2 = myArr.splice(1,5);
+
+console.log("C " , myArr);
+// console.log("C " , newArr);
+console.log(myn2);
+
+
