1a044d4da3d5219aa50f15d2c805559fcdc87bc0
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-08 17:49:31 +0000

    function

---
+++ 03_basics/functions.js
@@ -0,0 +1,24 @@
+function sayMyName () {
+    console.log("N");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName();
+
+function addTwoNum(num1 , num2){        // num1 & num2 are parameter 
+    console.log(num1 + num2);
+    
+}
+
+// addTwoNum(3,5)                          // 3 & 5 are arguments 
+// addTwoNum(3,"5")                          // 3 & 5 are arguments 
+// addTwoNum(3,"a")                          // 3 & 5 are arguments 
+// addTwoNum(3,null)                          // 3 & 5 are arguments 
+
+const result = addTwoNum(4, 5)
+
+console.log("Result is " , result);
