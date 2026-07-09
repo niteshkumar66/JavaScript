8799fdcc4a696a25df5b2f2602ad75340821be4f
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-09 08:57:24 +0000

    arrow function

---
+++ 03_basics/arrow_function.js
@@ -0,0 +1,19 @@
+const user = {
+    username : "nitesh",
+    price : 999,
+    
+    welcomeMessage : function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+        
+        
+    } 
+}
+
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
+
+// console.log(this);
+
+
