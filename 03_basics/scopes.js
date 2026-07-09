93b3f84089366173a2c4f8b5465c1c20076c464d
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-09 08:12:32 +0000

    scopes update

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -4,12 +4,53 @@ let a = 200
 if(true){               // local scope
     let a = 10 
     const b = 20
-    console.log("INNER" , a);
+    // console.log("INNER" , a);
     
 }
 
-
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Nitesh"
+
+    function two() {
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "nitesh"
+
+    if (username === "nitesh"){
+        const website = " youtube "
+        // console.log(username + website);
+
+    }
+    // console.log(Website);
+    
+}
+// console.log(username);
+
+
+// ********************************* Interesting ************************************
+
+
+console.log(addOne(5));                 // Can Accessed , no error 
+function addOne (num){
+    return num + 1
+}
+
+
+addTwo(5)                               // can't access , access befor initialisation error
+const addTwo = function(num) {
+    return num + 2
+}
