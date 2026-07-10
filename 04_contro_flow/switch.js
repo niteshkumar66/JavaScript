3a13c162ee83d6e83d8fa80a73853fd5834627a8
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-10 18:59:43 +0000

    switch

---
+++ 04_contro_flow/switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+
+switch (month) {
+    case 1:                         // Case sensitive 
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    default:
+        break;
+}
