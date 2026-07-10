6513c08a48278759fd0a213c58103c427203994f
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-10 04:03:30 +0000

    arrow function

--- 03_basics/arrow_function.js
+++ 03_basics/arrow_function.js
@@ -10,10 +10,52 @@ const user = {
     } 
 }
 
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
 
 // console.log(this);
 
+// function chai (){
+//     let username = "nitesh"
+//     console.log(this.username);
+    
+// }
 
+// chai()
+
+// const chai = function(){
+//     let username = "nitesh"
+//     console.log(this.username);
+    
+// }
+
+// Arrow Function
+
+const chai = () => {
+    let username = "nitesh"
+    console.log(this);
+    
+}
+
+// chai()
+
+
+// const addTwo = (num1 , num2) => {
+//     return (num1 + num2);                   // explicitily return
+    
+// }
+
+
+// const addTwo = (num1 , num2) => num1 + num2      // implicitily return
+
+// const addTwo = (num1 , num2) => (num1 + num2)
+
+const addTwo = (num1 , num2) => ({username : "nitesh"})
+
+console.log(addTwo());
+
+
+// const myArr = [3, 6, 7, 8, 1]
+
+//  myArr.forEach()
