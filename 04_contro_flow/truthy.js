a7f5be33b575a82b3079e10af80d88e1bdf6ca82
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-10 19:11:48 +0000

    truthy

---
+++ 04_contro_flow/truthy.js
@@ -0,0 +1,44 @@
+// const userEmail = "n@nitesh.com"
+
+// if (userEmail) {
+//     console.log("Got User Email");
+    
+// } else {
+//     console.log("Don't have User Email");
+    
+// }
+
+
+// const userEmail = ""
+
+// if (userEmail) {
+//     console.log("Got User Email");
+    
+// } else {
+//     console.log("Don't have User Email");
+    
+// }
+
+
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got User Email");
+    
+} else {
+    console.log("Don't have User Email");
+    
+}
+
+// falsy values 
+
+// false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN 
+
+// truthy value 
+
+// "0" , 'false' , " " , [] , {} , function(){} ,
+
+if(userEmail.length === 0){
+    console.log("Array is empty");
+    
+}
