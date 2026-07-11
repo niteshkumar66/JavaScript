b3dcbad90920e234825c8d044a54fd8ba9e217c3
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-11 18:09:00 +0000

    while loop

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,15 @@
+// while loop
+
+let i = 0
+while (i <= 10) {
+    // console.log(`value of index is ${i}`);
+    i+= 2
+}
+
+
+let myArr = [ 'flashman' , 'batman' , 'superman']
+let arr = 0;
+while (arr < myArr.length ) {
+    console.log(`value is ${myArr[arr]}`);
+    arr++
+}
