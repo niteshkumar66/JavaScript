7b34c94d6ea339ccb848eaa97476fcd4f374c74d
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-11 18:19:23 +0000

    do while loop

---
+++ 05_iterations/do_while_loop.js
@@ -0,0 +1,16 @@
+// do while loop
+
+
+// let score = 1
+// do {
+//     // console.log(`Score is ${score} `);
+//     score++
+// } while (score < 10);
+
+
+let score = 11
+do {
+    console.log(`Score is ${score} `);
+    score++
+} while (score < 10);
+
