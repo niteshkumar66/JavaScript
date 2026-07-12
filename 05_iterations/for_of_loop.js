961707a63ef423638a61830a70558cdf3a4543ed
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-12 17:01:32 +0000

    for of loop

--- 05_iterations/for_of_loop.js
+++ 05_iterations/for_of_loop.js
@@ -5,7 +5,38 @@
 
 const arr = [1, 2, 3, 4, 5]
 
-for (const num of arr) {
-    console.log(num);
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const greeting = "Hello World!"
+
+for (const greet of greeting) {
+    // console.log(`Each char is ${greet}`);  
+}
+
+// Maps 
+const map = new Map()
+
+map.set('IN', "INDIA")
+map.set('USA', "UNITED STATE OF AMERICA")
+map.set('FR', "FRANCE")
+map.set('IN', "INDIA")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(`${key} has value => ${value}`);
     
 }
+
+
+const myObj = {
+    game1 : "NFS",
+    game2 : "PUBG"
+}
+
+// for (const [key, value] of myObj) {
+//     console.log(key);
+    
+// }
