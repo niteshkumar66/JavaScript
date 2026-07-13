73a032f528e2fcb41458cd0b12656d211e9c130f
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-13 07:55:59 +0000

    for each loop

---
+++ 05_iterations/for_each_loop.js
@@ -0,0 +1,49 @@
+// for each loop
+
+["" , "" , ""]
+[{}, {}, {}]
+
+const coding = ['js' , 'ruby', 'py', 'java', 'cpp']
+
+// coding.forEach( function (item) {
+//     console.log(item);
+    
+// } )
+
+// coding.forEach( (items) => {
+//     console.log(items);
+    
+// } )
+
+
+// function printMe (item) {
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item , index , arr) => {
+//     console.log(item , index , arr);
+    
+// } )
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "python",
+        languageFileName : "py"
+    },
+    {
+        languageName : "java",
+        languageFileName : "java "
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageFileName);
+    
+} )
