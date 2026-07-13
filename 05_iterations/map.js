dc5208efd6b842b61f41de53db7f1c6ad00468d9
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-13 11:03:08 +0000

    map

---
+++ 05_iterations/map.js
@@ -0,0 +1,23 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.map( (num) => {
+//     return num+10
+// } )
+
+// console.log(newNums);
+
+const value = []
+myNums.forEach( (item) => {
+    value.push(item + 10)
+    
+} )
+
+// console.log(value);
+
+
+const newNums = myNums
+                    .map((num) => num * 10 )
+                    .map( (num) => num + 1)
+                    .filter( (num) => num >= 50)
+
+console.log(newNums);
