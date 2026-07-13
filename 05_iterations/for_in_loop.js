1fce428c408aeaa243ebb9a5a7dd60ca84a0cf86
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-13 07:36:42 +0000

    for in loop

--- 05_iterations/for_in_loop.js
+++ 05_iterations/for_in_loop.js
@@ -5,7 +5,25 @@ const myObj = {
     swift : "swift by apple "
 }
 
-for (const key in myObj) {
-    console.log(key);
-    
+// for (const key in myObj) {
+//      console.log(myObj[key]);
+//      console.log(`${key} has the value : ${myObj[key]}`);
+// }
+
+const programming = ['js' , 'java' , 'py', 'cpp']
+
+for (const key in programming) {
+    // console.log(programming[key]);
 }
+
+// const map = new Map()
+// map.set('IN', "INDIA")
+// map.set('USA', "UNITED STATE OF AMERICA")
+// map.set('FR', "FRANCE")
+// map.set('IN', "INDIA")
+
+// for (const key in map) {
+//     console.log(map[key]);
+    
+// }
+
