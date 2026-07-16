b0327bf2de65a31703247c5e99fda1e1d5674e0c
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-16 18:35:51 +0000

    project 2 complete

--- projects/project_2/project2.js
+++ projects/project_2/project2.js
@@ -8,15 +8,26 @@ form.addEventListener('submit', function(e) {
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#result')
+    let bmi;
 
     if(height === '' || height < 0 || isNaN(height)){
         result.innerHTML = `please give a valid height ${height}`
     } else if (weight === '' || weight < 0 || isNaN(weight)){
         result.innerHTML = `please give a valid weight ${weight}`
     } else {
-        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        bmi = (weight / ((height*height)/10000)).toFixed(2)
         // show the result 
 
         result.innerHTML= `<span>${bmi}</span>`
+    
     }
+
+    if (bmi < 18.6){
+            result.appendChild(document.createTextNode(` Under Weight`))
+        } else if (bmi >= 18.6 && bmi < 24.9){
+            result.appendChild(document.createTextNode(` Normal Weight`))
+        } else {
+            result.appendChild(document.createTextNode(` Over Weight`))
+        }
+
 })
