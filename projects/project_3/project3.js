049b7e873ceb746996eb1e82c0640d3a9aee75d5
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-17 07:22:05 +0000

    project 3

--- projects/project_3/project3.js
+++ projects/project_3/project3.js
@@ -1,8 +1,10 @@
-const clock = document.getElementById('clock') 
+const clock = document.getElementById('clock')
 
-
-setInterval(function (){
-    let date = new Date()
+setInterval( function() {
+    const date = new Date()
     // console.log(date.toLocaleTimeString());
+
     clock.innerHTML = date.toLocaleTimeString()
-} , 1000)
+    
+} , 1000 )
+
