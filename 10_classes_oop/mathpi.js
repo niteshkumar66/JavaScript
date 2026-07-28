38cf124b9b6a16c7bc50a5d2022bdece72a9ed12
NITESH KUMAR <nitesh0221kumar@gmail.com>
2026-07-28 17:07:26 +0000

    mathpi

--- 10_classes_oop/mathpi.js
+++ 10_classes_oop/mathpi.js
@@ -34,6 +34,5 @@ console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 for (let [key, value] of Object.entries(chai)) {
     if (typeof value !== 'function') {
         console.log(`${key} : ${value}`);
-    }
-    
+    } 
 }
